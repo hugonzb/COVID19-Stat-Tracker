10cdad5cf6d800b84fdf3b7eeef6422cd4a19f75
Hugo Baird <hugonzb@gmail.com>
2020-06-28 00:33:20 +1200

    Added styling to cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'; 
 import CountUp from 'react-countup';
-
 import styles from './Cards.module.css';
-import CountryPicker from '../CountryPicker/CountryPicker';
+import cx from 'classnames';
+
 const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
     if(!confirmed) {
         return 'Loading Statistics...';
@@ -11,7 +11,7 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center"> 
-                <Grid item component ={Card}>
+                <Grid item component ={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5"><CountUp start={0} end={confirmed.value} duration={3} separate=","/>
@@ -20,7 +20,7 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
                         <Typography variant="body2">Number of active cases</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component ={Card}>
+                <Grid item component ={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5"><CountUp start={0} end={recovered.value} duration={3} separate=","/></Typography>
@@ -28,7 +28,7 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
                         <Typography variant="body2">Number of recoveries</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component ={Card}>
+                <Grid item component ={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5"><CountUp start={0} end={deaths.value} duration={3} separate=","/></Typography>
