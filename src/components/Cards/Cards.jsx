bf437f6e3af69096194ac916b8cbdd693d58779a
Hugo Baird <hugonzb@gmail.com>
2020-06-27 23:34:55 +1200

    Implemented count up feature for infected

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'; 
+import CountUp from 'react-countup';
 
 import styles from './Cards.module.css';
-const Cards = (props) => {
-    console.log(props);
+import CountryPicker from '../CountryPicker/CountryPicker';
+const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
+    if(!confirmed) {
+        return 'Loading Statistics...';
+    }
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center"> 
                 <Grid item component ={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5"><CountUp start={0} end={confirmed.value} duration={3} separate=","/>
+                        </Typography>
+                        <Typography color="textSecondary">DATE</Typography>
+                        <Typography variant="body2">Number of active cases</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component ={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">DATA</Typography>
+                        <Typography color="textSecondary">DATE</Typography>
+                        <Typography variant="body2">Number of recoveries</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component ={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5">DATA</Typography>
+                        <Typography color="textSecondary">DATE</Typography>
+                        <Typography variant="body2">Number of deaths</Typography>
                     </CardContent>
                 </Grid>
             </Grid>
