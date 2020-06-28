0c3b62c77ecf8bb5cc9a4476f267200c1cf9dd74
Hugo Baird <hugonzb@gmail.com>
2020-06-28 17:33:22 +1200

    Added footer with links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
+import linkedin from './images/linkedin.png';
+import github from './images/github.png';
+import email from './images/email.png';
 
 class App extends React.Component {
     state = {
@@ -27,13 +30,25 @@ class App extends React.Component {
                 <div className = {styles.title}>
                     COVID-19 Statistic Tracker
                 </div>
+                <div className = {styles.subtitle}>
+                    Developed by Hugo Baird
+                </div>
+                <div className = {styles.subtitle}>
+                    Shoutout to Adrian Hajdin @JavaScript Mastery
+                </div>
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Cards data={data}/>
                 <Chart data={data} country={country}/>
-                <div className ={styles.footer}></div>
+                <div className ={styles.footer}>
+                        <a href="https://github.com/hugonzb"><img className ={styles.github} src={github} alt="github"></img></a>
+                        <a href="https://www.linkedin.com/in/hugo-baird/"><img className ={styles.img} src={linkedin} alt="linkedin"></img></a>
+                        <img className ={styles.img} src={email} alt="email" ></img><div className ={styles.email}>hugonzb@gmail.com</div>
+                </div>
             </div>    
         )
     }
 }
+
+
 
 export default App;
