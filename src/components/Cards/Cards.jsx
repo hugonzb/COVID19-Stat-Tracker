1da6abb3d6322dd988fcbd1b5f8390fa58a4f026
Hugo Baird <hugonzb@gmail.com>
2020-06-28 16:16:07 +1200

    Added icons for cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'; 
 import CountUp from 'react-countup';
 import styles from './Cards.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeadSideVirus, faHeart, faSkull } from "@fortawesome/free-solid-svg-icons";
 import cx from 'classnames';
 
 const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
@@ -15,6 +17,7 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
             <Grid container spacing={3} justify="center"> 
                 <Grid item component ={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
+                        <FontAwesomeIcon icon={faHeadSideVirus}/>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5"><CountUp start={0} end={confirmed.value} duration={3} separator=","/>
                         </Typography>
@@ -24,6 +27,7 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
                 </Grid>
                 <Grid item component ={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
+                        <FontAwesomeIcon icon={faHeart}/>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5"><CountUp start={0} end={recovered.value} duration={3} separator=","/></Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
@@ -32,6 +36,7 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
                 </Grid>
                 <Grid item component ={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
+                        <FontAwesomeIcon icon={faSkull}/>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5"><CountUp start={0} end={deaths.value} duration={3} separator=","/></Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
